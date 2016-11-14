a4d7700856ad4499770b6f0f2060146a78275d10
Fredrik Olofsson <frol@hupp.se>
2016-11-14 19:09:33 +0100

    Add Emacs files.

---
+++ .emacs.d/init.el
@@ -0,0 +1,18 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(blink-cursor-mode nil)
+ '(column-number-mode t)
+ '(global-whitespace-mode t)
+ '(save-place t nil (saveplace))
+ '(show-paren-mode t)
+ '(tool-bar-mode nil)
+ '(whitespace-style (quote (face trailing space-before-tab empty))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
